0d0e30c0c767d531e60de630c6b0ca79a30d3a2a
Nick Murdoch <nick@nivan.net>
2013-02-15 23:21:47 +0000

    Turn beep into a function, chmod +x & hashbang.

--- physio.py
+++ physio.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python
+import sys
 import time
+
+def beep():
+    sys.stdout.write('\a')
+    sys.stdout.flush()
 
 for i in range(6):
     for j in range(10):
         for k in range(5):
             print i + 1, j + 1, k + 1
-            print '\a'
+            beep()
             if k == 4 and j == 9:
                 time.sleep(0.2)
-                print '\a'
+                beep()
             time.sleep(1)
         time.sleep(1)
     time.sleep(4)
