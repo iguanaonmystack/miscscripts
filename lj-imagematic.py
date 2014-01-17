378d051f73e7d27f862a8f3ed7de687998f8987c
Nick Murdoch <nick@nivan.net>
2014-01-17 19:21:48 +0000

    Update for new journal format.

--- lj-imagematic.py
+++ lj-imagematic.py
@@ -4,13 +4,21 @@ import sys
 # Simple script to allow me to just copy image links into vim
 # and have this script turn them into actual images.
 
-fmt = '<a href="%s"><img src="%s" /></a>'
+fmt = '<a href="%s"><img src="%s" style="border: 2px solid black;" /></a>'
 
 for line in sys.stdin:
     line = line.strip()
     if line.startswith('http') and line.lower().endswith('.jpg'):
+        imgurl = linkurl = line
         if '.small.' in line:
-            line = fmt % (line, line.replace('.small.', '.thumb.'))
+            imgurl = line.replace('.small.', '.lj.')
         elif '.thumb.' in line:
-            line = fmt % (line.replace('.thumb.', '.small.'), line)
+            imgurl = line.replace('.thumb.', '.lj.')
+            linkurl = line.replace('.thumb.', '.small.')
+        elif '.lj.' in line:
+            linkurl = line.replace('.lj.', '.small.')
+        else:
+            linkurl = line[:-4] + '.small' + line[-4:]
+            imgurl = line[:-4] + '.lj' + line[-4:]
+        line = fmt % (linkurl, imgurl)
     print line
