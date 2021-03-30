7d9910ca379f761ed30e6a0f3a27bea35ba01eb1
Kathy Murdoch <kathy@nivan.net>
2021-03-30 21:14:41 +0100

    Fix aspect ratio

--- dslr-webcam.sh
+++ dslr-webcam.sh
@@ -1,3 +1,3 @@
 lsmod | grep ^v4l2loopback || sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
 
-gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -s 1280x720 -f v4l2 /dev/video2
+gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -s 1080x720 -f v4l2 /dev/video2
